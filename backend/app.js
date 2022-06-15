a795b61c2ff3cf9b848430fe571a49fe9020b74e
Rahul Goyal <rahulgoyal01052003@gmail.com>
2022-06-15 19:55:42 +0530

    static file path modified

--- backend/app.js
+++ backend/app.js
@@ -105,10 +105,10 @@ app.get('/:hash',async (req,res)=>{
     }
 })
 
-app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../build/index.html"));
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 // let process={env:{PORT:4000}}
 const server =app.listen(process.env.PORT||4000,()=>{
